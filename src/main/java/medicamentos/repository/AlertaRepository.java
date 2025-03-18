7d20a7f8d1395fe5c25615068caf5f55a2070922
valeria <valeriaclemente271@gmail.com>
2025-03-18 17:10:08 +0100

    (feat): garegando nuevo metodo paa comrpoabr si aun hay alertas pendientes

--- src/main/java/medicamentos/repository/AlertaRepository.java
+++ src/main/java/medicamentos/repository/AlertaRepository.java
@@ -1,10 +1,21 @@
 package medicamentos.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import medicamentos.entities.Alerta;
+import medicamentos.entities.Medicamento;
+import medicamentos.entities.Paciente;
 
 public interface AlertaRepository extends JpaRepository<Alerta, Integer>{
 	
+	@Query("SELECT COUNT(a) FROM Alerta a WHERE a.medicamento = :medicamento AND a.paciente = :paciente AND a.fechaHoraAlerta > :fechaActual AND a.estadoAlerta = 'sinConfirmar'")
+	long countByMedicamentoAndPacienteAndFechaHoraAlertaAfter(@Param("medicamento") Medicamento medicamento, 
+	                                                          @Param("paciente") Paciente paciente, 
+	                                                          @Param("fechaActual") LocalDateTime fechaActual);
+
 
 }
